077d4c3b9ea4db6fa09b89727e1b531f1bd08d73
Bruno <b.g.santos@outlook.com>
2019-11-09 13:55:09 -0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pocket-mongo-bgss",
+    version="0.0.1",
+    author="Bruno Gonçalves dos Santos",
+    author_email="brunogoncalves.santos@gmail.com",
+    description="An easy-to-use and quick setup interface for MongoDB.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gssbruno/pocket-mongo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
